092af08f80c82f243a4aeddf2a209de580a8cde4
zaidaanfarros615 <zaidaanfarros615@gmail.com>
2026-04-03 09:32:23 +0700

    Upload kode aplikasi sepatu

---
+++ app.py
@@ -0,0 +1,11 @@
+
+import cv2
+import numpy as np
+import json
+import os
+
+def engine_ai_sepatu(image_path):
+    return "Hasil Analisis"
+
+if __name__ == "__main__":
+    print("Aplikasi Web Sepatu Siap!")
